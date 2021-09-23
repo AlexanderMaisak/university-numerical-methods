c23405e5373b44ff4ce33c62e06de52e88179c77
Alexander Maisak <sashamaisak094@gmail.com>
2021-09-24 00:15:50 +0300

    FILL custom arrays

--- src/main/java/com/university/lab/app/Application.java
+++ src/main/java/com/university/lab/app/Application.java
@@ -1,9 +1,91 @@
 package com.university.lab.app;
 
+import java.util.Scanner;
+
 public class Application {
+
+    public static final int COLS_AND_ROWS_VALUE = 3;
 
     public static void main(String[] args) {
 
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter the number of cols");
+//        int cols = in.nextInt();
+//        System.out.println("Enter the number of rows");
+//        int rows = in.nextInt();
+//
+//        int[][] arrA = new int[rows][cols];
+//        int[] vecB = new int[rows];
+//        defaultFillingArrayAndVector(vectorB, in, cols, rows, arrA);
+//
+//        in.close();
+
+        int[][] arrayA = new int[COLS_AND_ROWS_VALUE][COLS_AND_ROWS_VALUE];
+        int[] vectorB = new int[COLS_AND_ROWS_VALUE];
+
+        testFillingArrayAndVectorForTask(arrayA, vectorB);
+
+    }
+
+    private static void defaultFillingArrayAndVector(int[] vectorB, Scanner in, int cols, int rows, int[][] arrA) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arrA[i][j] = in.nextInt();
+            }
+        }
+
+        System.out.println("Matrix A:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.printf("%s\t", arrA[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 0; i < rows; i++) {
+            vectorB[i] = in.nextInt();
+        }
+
+        System.out.println("vector b:");
+        for (int i = 0; i < rows; i++) {
+            System.out.printf("%s\t", vectorB[i]);
+        }
+        System.out.println();
+    }
+
+    private static void testFillingArrayAndVectorForTask(int[][] arrayA, int[] vectorB) {
+        arrayA[0][0] = 1;
+        arrayA[0][1] = 2;
+        arrayA[0][2] = 1;
+
+        arrayA[1][0] = -1;
+        arrayA[1][1] = -2;
+        arrayA[1][2] = -2;
+
+        arrayA[2][0] = 0;
+        arrayA[2][1] = 1;
+        arrayA[2][2] = 1;
+
+        vectorB[0] = 1;
+        vectorB[1] = 1;
+        vectorB[2] = 2;
+
+        System.out.println("Matrix A:");
+        for (int i = 0; i < COLS_AND_ROWS_VALUE; i++) {
+            for (int j = 0; j < COLS_AND_ROWS_VALUE; j++) {
+                System.out.printf("%s\t", arrayA[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("vector b:");
+        for (int i = 0; i < COLS_AND_ROWS_VALUE; i++) {
+            System.out.printf("%s\t", vectorB[i]);
+        }
+        System.out.println();
+        System.out.println();
     }
 
 }
---
+++ src/main/java/com/university/lab/service/Gauss.java
@@ -0,0 +1,13 @@
+package com.university.lab.model;
+
+public interface Gauss {
+
+    void addEquation(int item);
+
+    void multiply(int coefficient);
+
+    int findCoefficient(int a, int b);
+
+    int retrieve(int index);
+
+}
